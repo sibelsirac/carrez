95ca452c499ea9865dffb43b8f435a7f3ebc9434
sibelsirac <sibel.sirac@devinci.fr>
2017-01-20 14:35:43 +0100

    node et leboncoin meilleursagents json

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var index = path.resolve(__dirname, './index.html');
+
+console.log('Listening!');
+
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+app.get('/test', function(req, res){
+    res.send('nous sommes dans test');
+});
+app.listen(3000);
